a2dc28065e24761b414a06b330fc24fe701845d7
WangChong <mz1874@aliyun.com>
2025-08-07 13:12:29 +0800

    # Added feature 1- finished everywhere LineCharts

--- src/views/everywhere/index.vue
+++ src/views/everywhere/index.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import {ArrowRightOutlined} from '@ant-design/icons-vue';
-import {h, onMounted, ref} from 'vue';
+import {h, nextTick, onMounted, ref, watch} from 'vue';
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import * as echarts from 'echarts'
-import type { ECharts } from 'echarts'
+import type {ECharts} from 'echarts'
+
+type EChartsOption = echarts.EChartsOption;
 
 // 多个 chart ref
 const priceChartRef = ref<HTMLElement | null>(null)
@@ -16,6 +18,22 @@ const priceChartRef5 = ref<HTMLElement | null>(null)
 const priceChartRef6 = ref<HTMLElement | null>(null)
 const priceChartRef7 = ref<HTMLElement | null>(null)
 const priceChartRef8 = ref<HTMLElement | null>(null)
+
+const stackedArea1 = ref<HTMLElement | null>(null)
+const stackedArea2 = ref<HTMLElement | null>(null)
+const stackedArea3 = ref<HTMLElement | null>(null)
+const stackedArea4 = ref<HTMLElement | null>(null)
+
+
+const lineChart1 = ref<HTMLElement | null>(null)
+const lineChart2 = ref<HTMLElement | null>(null)
+const lineChart3 = ref<HTMLElement | null>(null)
+const lineChart4 = ref<HTMLElement | null>(null)
+
+
+// var myChart = echarts.init(stackedArea1.value);
+// var option: EChartsOption;
+
 
 const handleCityChange = () => {
 
@@ -177,6 +195,297 @@ const data = [...Array(8)].map((_, i) => ({
 const activeKey = ref('1');
 
 
+var option: EChartsOption;
+
+option = {
+  color: ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00'],
+  title: {
+    text: 'Gradient Stacked Area Chart'
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      label: {
+        backgroundColor: '#6a7985'
+      }
+    }
+  },
+  legend: {
+    data: ['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5']
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {},
+      fullScreen: {}
+    }
+  },
+  xAxis: [
+    {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    }
+  ],
+  yAxis: [
+    {
+      type: 'value'
+    }
+  ],
+  series: [
+    {
+      name: 'Line 1',
+      type: 'line',
+      stack: 'Total',
+      smooth: true,
+      lineStyle: {
+        width: 0
+      },
+      showSymbol: false,
+      areaStyle: {
+        opacity: 0.8,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(128, 255, 165)'
+          },
+          {
+            offset: 1,
+            color: 'rgb(1, 191, 236)'
+          }
+        ])
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [140, 232, 101, 264, 90, 340, 250]
+    },
+    {
+      name: 'Line 2',
+      type: 'line',
+      stack: 'Total',
+      smooth: true,
+      lineStyle: {
+        width: 0
+      },
+      showSymbol: false,
+      areaStyle: {
+        opacity: 0.8,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(0, 221, 255)'
+          },
+          {
+            offset: 1,
+            color: 'rgb(77, 119, 255)'
+          }
+        ])
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [120, 282, 111, 234, 220, 340, 310]
+    },
+    {
+      name: 'Line 3',
+      type: 'line',
+      stack: 'Total',
+      smooth: true,
+      lineStyle: {
+        width: 0
+      },
+      showSymbol: false,
+      areaStyle: {
+        opacity: 0.8,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(55, 162, 255)'
+          },
+          {
+            offset: 1,
+            color: 'rgb(116, 21, 219)'
+          }
+        ])
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [320, 132, 201, 334, 190, 130, 220]
+    },
+    {
+      name: 'Line 4',
+      type: 'line',
+      stack: 'Total',
+      smooth: true,
+      lineStyle: {
+        width: 0
+      },
+      showSymbol: false,
+      areaStyle: {
+        opacity: 0.8,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(255, 0, 135)'
+          },
+          {
+            offset: 1,
+            color: 'rgb(135, 0, 157)'
+          }
+        ])
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [220, 402, 231, 134, 190, 230, 120]
+    },
+    {
+      name: 'Line 5',
+      type: 'line',
+      stack: 'Total',
+      smooth: true,
+      lineStyle: {
+        width: 0
+      },
+      showSymbol: false,
+      label: {
+        show: true,
+        position: 'top'
+      },
+      areaStyle: {
+        opacity: 0.8,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(255, 191, 0)'
+          },
+          {
+            offset: 1,
+            color: 'rgb(224, 62, 76)'
+          }
+        ])
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [220, 302, 181, 234, 210, 290, 150]
+    }
+  ]
+};
+
+var lineChartOptions : EChartsOption;
+
+lineChartOptions = {
+  title: {
+    text: 'Stacked Line'
+  },
+  tooltip: {
+    trigger: 'axis'
+  },
+  legend: {
+    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
+    containLabel: true
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '20%',
+    containLabel: true
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {}
+    }
+  },
+  xAxis: {
+    type: 'category',
+    boundaryGap: false,
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      name: 'Email',
+      type: 'line',
+      stack: 'Total',
+      data: [120, 132, 101, 134, 90, 230, 210]
+    },
+    {
+      name: 'Union Ads',
+      type: 'line',
+      stack: 'Total',
+      data: [220, 182, 191, 234, 290, 330, 310]
+    },
+    {
+      name: 'Video Ads',
+      type: 'line',
+      stack: 'Total',
+      data: [150, 232, 201, 154, 190, 330, 410]
+    },
+    {
+      name: 'Direct',
+      type: 'line',
+      stack: 'Total',
+      data: [320, 332, 301, 334, 390, 330, 320]
+    },
+    {
+      name: 'Search Engine',
+      type: 'line',
+      stack: 'Total',
+      data: [820, 932, 901, 934, 1290, 1330, 1320]
+    }
+  ]
+};
+
+
+watch(activeKey, async (val) => {
+  console.log(val)
+  if (val === '2') {
+    await nextTick(); // 等待 DOM 更新完
+    if (stackedArea1.value) {
+      const myChart = echarts.init(stackedArea1.value);
+      option && myChart.setOption(option);
+      myChart.resize();
+
+      const myChart2 = echarts.init(stackedArea2.value);
+      option && myChart2.setOption(option);
+      myChart2.resize();
+
+      const myChart3 = echarts.init(stackedArea3.value);
+      option && myChart3.setOption(option);
+      myChart3.resize();
+
+      const myChart4 = echarts.init(stackedArea4.value);
+      option && myChart4.setOption(option);
+      myChart4.resize();
+    }
+  } else if (val === '3') {
+    await nextTick();
+    const MyLineChart1 = echarts.init(lineChart1.value);
+    option && MyLineChart1.setOption(lineChartOptions);
+    MyLineChart1.resize();
+
+    const MyLineChart2 = echarts.init(lineChart2.value);
+    option && MyLineChart2.setOption(lineChartOptions);
+    MyLineChart2.resize();
+
+    const MyLineChart3 = echarts.init(lineChart3.value);
+    option && MyLineChart3.setOption(lineChartOptions);
+    MyLineChart3.resize();
+
+    const MyLineChart4 = echarts.init(lineChart4.value);
+    option && MyLineChart4.setOption(lineChartOptions);
+    MyLineChart4.resize();
+
+  }
+});
+
+
+
 const mapContainerId = 'geojson-map'
 
 
@@ -283,6 +592,8 @@ onMounted(async () => {
   renderChart(priceChartRef6.value, priceOption)
   renderChart(priceChartRef7.value, priceOption)
   renderChart(priceChartRef8.value, priceOption)
+
+
 });
 </script>
 
@@ -442,8 +753,38 @@ onMounted(async () => {
             </a-col>
           </a-row>
         </a-tab-pane>
-        <a-tab-pane key="2" tab="Fuel Mix" force-render>Content of Tab Pane 2</a-tab-pane>
-        <a-tab-pane key="3" tab="Load">Content of Tab Pane 3</a-tab-pane>
+        <a-tab-pane key="2" tab="Fuel Mix" force-render>
+          <a-row>
+            <a-col :span="6">
+              <div ref="stackedArea1" style="width: 100%; height: 400px;"></div>
+            </a-col>
+            <a-col :span="6">
+              <div ref="stackedArea2" style="width: 100%; height: 400px;"></div>
+            </a-col>
+            <a-col :span="6">
+              <div ref="stackedArea3" style="width: 100%; height: 400px;"></div>
+            </a-col>
+            <a-col :span="6">
+              <div ref="stackedArea4" style="width: 100%; height: 400px;"></div>
+            </a-col>
+          </a-row>
+        </a-tab-pane>
+        <a-tab-pane key="3" tab="Load" force-render>
+          <a-row>
+            <a-col :span="6">
+              <div ref="lineChart1" style="width: 100%; height: 400px;"></div>
+            </a-col>
+            <a-col :span="6">
+              <div ref="lineChart2" style="width: 100%; height: 400px;"></div>
+            </a-col>
+            <a-col :span="6">
+              <div ref="lineChart3" style="width: 100%; height: 400px;"></div>
+            </a-col>
+            <a-col :span="6">
+              <div ref="lineChart4" style="width: 100%; height: 400px;"></div>
+            </a-col>
+          </a-row>
+        </a-tab-pane>
         <a-tab-pane key="4" tab="Renewables">Content of Tab Pane 3</a-tab-pane>
       </a-tabs>
     </a-col>
